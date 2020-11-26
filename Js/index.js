dbd97d8b1ac6d1ed365f041636c2a9424c92a4c1
Asurg17 <48888626+Asurg17@users.noreply.github.com>
2020-11-26 22:35:07 +0400

    slider added

---
+++ Js/index.js
@@ -0,0 +1,46 @@
+// ----
+
+let maxSize = 6;
+let num = 0;
+
+function slideLeft() {
+
+	let arr = document.querySelectorAll(".browse_cars_style");
+	let size = arr.length;
+
+	arr[(num)%size].style.display = "none";
+
+	for (i=0; i<maxSize; i++){
+		arr[(num + 1 + i)%size].style.order = i+1;
+		arr[(num + 1 + i)%size].style.display = "flex";
+	}
+
+	num = num + 1;
+}
+
+function slideRight() {
+
+	let arr = document.querySelectorAll(".browse_cars_style");
+	let size = arr.length;
+
+	index = (num+(maxSize-1))%size;
+
+	arr[index].style.display = "none";
+
+	index = index - 6;
+
+	if(index < 0){
+		index = size + index;
+	}
+
+	for (i=0; i<maxSize; i++){
+		arr[(index+i)%size].style.order = i+1;
+		arr[(index+i)%size].style.display = "flex";
+	}	
+	if(num == 0){
+		num = size - 1;
+	}else{
+		num = num - 1;
+	}
+
+}
