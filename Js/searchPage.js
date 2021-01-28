dc9bf9a4f58ae84a3efd1684014c118c2cc3d577
Asurg17 <48888626+Asurg17@users.noreply.github.com>
2021-01-28 22:29:44 +0400

    added js & some needed changes

--- Js/searchPage.js
+++ Js/searchPage.js
@@ -1,6 +1,25 @@
 
 // ---------------------------------------------------------------
 
+window.onload = function(){
+
+	document.getElementById("filter_button").addEventListener("click", showFilter);
+
+	document.getElementById("search_car").addEventListener("click", searchCar);
+
+ 	document.getElementById("manufacturer").addEventListener("click", getCarModels);
+
+ 	getCarFilterInfo();
+
+ 	checkWindowSize();
+
+}
+
+window.addEventListener("resize", checkWindowSize);
+
+// -------------------------------------------------------------------------------
+
+
 //needs to get info it filter must be shown or hidden
 checker = 1; 
 maxHeight = 450;
@@ -19,20 +38,6 @@ function showFilter(){
 		document.getElementById("sub_filter").style.visibility = "hidden";
 	}
 }
-
-// ########
-
-window.onload = function(){
-
-	var carSearchBut = document.getElementById("search_car");
- 	
- 	carSearchBut.addEventListener("click", searchCar);
-
- 	checkWindowSize();
-
-}
-
-window.addEventListener("resize", checkWindowSize);
 
 
 function checkWindowSize(){
@@ -58,8 +63,147 @@ function checkWindowSize(){
 
 }
 
+// -------------------------------------------------------------------
+
+function getCarFilterInfo(){
+
+	fetch('../Json/SearchPage/carCategories.json')
+		.then(response => response.json())
+		.then((result) => {setData(result, "categories")});
+
+	fetch('../Json/SearchPage/manufacturers.json')
+		.then(response => response.json())
+		.then((result) => {setData(result, "manufacturer")});
+
+	fetch('../Json/SearchPage/carEngine.json')
+		.then(response => response.json())
+		.then((result) => {setData(result, "engine_from"), setData(result, "engine_to")});
+
+	fetch('../Json/SearchPage/carTransmissions.json')
+		.then(response => response.json())
+		.then((result) => {setData(result, "transmission_filtr")});
+
+	fetch('../Json/SearchPage/fuel.json')
+		.then(response => response.json())
+		.then((result) => {setData(result, "fuel_filtr")});
+
+	fetch('../Json/SearchPage/helm.json')
+		.then(response => response.json())
+		.then((result) => {setData(result, "helm_filtr")});
+
+	fetch('../Json/SearchPage/seats.json')
+		.then(response => response.json())
+		.then((result) => {setData(result, "sealts_filtr")});
+
+}
+
+function setData(data, id){
+
+	var categorySelect = document.getElementById(id);
+
+	for(var i=0; i<data.length; i++){
+
+		var curOption = document.createElement('option');
+
+		curOption.appendChild(document.createTextNode(data[i]));
+
+		curOption.value = data[i];
+
+		categorySelect.appendChild(curOption); 
+
+	}
+
+}
+
+// ####################
+
+function getCarModels(){
+
+	fetch('../Json/SearchPage/carModels.json')
+		.then(response => response.json())
+		.then((result) => {showCarModels(result)});
+
+}
+
+
+var curManufacturer = "";
+
+function showCarModels(data){
+
+	var modelsSelect = document.getElementById("models");
+	var manufacturer = document.getElementById("manufacturer").value;
+
+	if(manufacturer == "none"){
+		
+		curManufacturer = manufacturer;
+		clearAllData(modelsSelect);
+
+	}else if(curManufacturer != manufacturer){
+
+		curManufacturer = manufacturer; 
+
+		var carModels;
+		var index = 0;
+
+		while(true){
+
+			if(data[index].manufacturer == manufacturer){
+				carModels = data[index].models;
+				break;
+			}
+
+			index ++;
+
+		}
+
+		clearAllData(modelsSelect);
+
+		for(var i=0; i<carModels.length; i++){
+
+			var curOption = document.createElement('option');
+
+			curOption.appendChild(document.createTextNode(carModels[i]));
+
+			curOption.value = carModels[i];
+
+			modelsSelect.appendChild(curOption); 
+
+		}
+
+	}
+
+}
+
+function clearAllData(select){
+
+	for(i=select.length-1; i>0; i--){
+		select.removeChild(select.options[i]);
+	}
+
+}
+
+
 function searchCar(){
 
 	alert("va va! ras edzeb dzma? :)");
+
+	var category = document.getElementById("categories").value;
+	var manufacturer = document.getElementById("manufacturer").value;
+	var model = document.getElementById("models").value;
+	
+	var priceFrom = document.getElementById("p_from").value;
+	var priceTo = document.getElementById("p_to").value;
+	var yearFrom = document.getElementById("y_from").value;
+	var yearTo = document.getElementById("y_to").value;
+	var engineFrom = document.getElementById("engine_from").value;
+	var engineTo = document.getElementById("engine_to").value;
+
+	var transmission = document.getElementById("transmission_filtr").value;
+	var fuel = document.getElementById("fuel_filtr").value;
+	var helm = document.getElementById("helm_filtr").value;
+	var seats = document.getElementById("sealts_filtr").value;
+	var color =  document.getElementById("color_filtr").value ;
+
+	console.log(category, manufacturer, model, priceFrom, priceTo, yearFrom, yearTo, engineFrom, engineTo, transmission, fuel, helm, seats, color);
 
 }
