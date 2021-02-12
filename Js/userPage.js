3e707f114e80bd3fb7f900dae141a386e3938d40
Asurg17 <48888626+Asurg17@users.noreply.github.com>
2021-02-12 18:26:33 +0400

    added new functions to userJson

--- Js/userPage.js
+++ Js/userPage.js
@@ -4,17 +4,6 @@
 
 window.onload = function(){
 
-	document.getElementById("header_logo").addEventListener("click", redirectToMainPage);
-
-	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-	document.getElementById("add_car").addEventListener("click", openPopupForm);
-	document.getElementById("remove_car").addEventListener("click", openClosePopupForm);
-	document.getElementById("cange_password").addEventListener("click", openChPswPopupForm);
-	document.getElementById("change_pdata").addEventListener("click", openChPopupForm);
-
-	// ---
-
 	var url = document.location.href, 
 	    params = url.split('?')[1].split('&'),
         data = {}, 
@@ -27,7 +16,27 @@ window.onload = function(){
 
 	getUserInfo(data.id);
 
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+	document.getElementById("header_logo").addEventListener("click", redirectToMainPage);
+
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+	document.getElementById("add_car").addEventListener("click", openPopupForm);
+	document.getElementById("remove_car").addEventListener("click", openClosePopupForm);
+	document.getElementById("cange_password").addEventListener("click", openChPswPopupForm);
+	document.getElementById("change_pdata").addEventListener("click", openChPopupForm);
+
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+	document.getElementById("b_add_car").addEventListener("click", addCar);
+	document.getElementById("b_remove_car").addEventListener("click", removeCar);
+	document.getElementById("b_change_password").addEventListener("click", changePassword);
+	document.getElementById("b_change_pdata").addEventListener("click", changePersonalData);
+
 }
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 function redirectToMainPage(){
 
@@ -35,6 +44,9 @@ function redirectToMainPage(){
 
 }
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+var curUser;
 
 function getUserInfo(id){
 
@@ -47,21 +59,21 @@ function getUserInfo(id){
 
 function setUserInfo(data, id){
 
-	var curUser = data[id];
+	curUser = data[id];
 
-	var username = curUser.username;
-	var whole_name = curUser.first_name + " " + curUser.last_name;
-	var contact = "(+995) " + curUser.phone;
-	var location = "Georgia, " + curUser.location;
-	var raiting = curUser.raiting;
-	var adverts = curUser.adverts;
+	// var username = curUser.username;
+	// var whole_name = curUser.first_name + " " + curUser.last_name;
+	// var contact = "(+995) " + curUser.phone;
+	// var location = "Georgia, " + curUser.location;
+	// var raiting = curUser.raiting;
+	// var adverts = curUser.adverts;
 
-	document.getElementById("username").innerHTML = username;
-	document.getElementById("u_name").innerHTML = whole_name;
-	document.getElementById("u_contact").innerHTML = contact;
-	document.getElementById("u_location").innerHTML = location;
-	document.getElementById("u_raiting").innerHTML = raiting;
-	document.getElementById("u_adverts").innerHTML = adverts;
+	// document.getElementById("username").innerHTML = username;
+	// document.getElementById("u_name").innerHTML = whole_name;
+	// document.getElementById("u_contact").innerHTML = contact;
+	// document.getElementById("u_location").innerHTML = location;
+	// document.getElementById("u_raiting").innerHTML = raiting;
+	// document.getElementById("u_adverts").innerHTML = adverts;
 
 }
 
@@ -104,4 +116,105 @@ function closeEveryForm(){
 	document.body.classList.remove("showRemoveCarForm");
 	document.body.classList.remove("showChAddCarForm");
 	document.body.classList.remove("showChPasswordForm");
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+function addCar(){
+
+	fetch('../Json/cars.json')
+		.then(response => response.json())
+		.then((result) => {add(result)});
+
+}
+
+function add(data){
+
+	var newCarId = data.length+1;
+
+	var newCarName = document.getElementById("car_name").value;
+	var newCarManufacturer = document.getElementById("manufacturer").value;
+	var newCarModel = document.getElementById("model").value;
+	var newCarModelYear = document.getElementById("model_year").value;
+ 	var newCarCategory = document.getElementById("category").value;
+ 	var newCarEngine = document.getElementById("engine").value;
+ 	var newCarTransmission = document.getElementById("transmission").value;
+ 	var newCarHelm = document.getElementById("helm").value;
+ 	var newCarFuel = document.getElementById("fuel").value;
+ 	var newCarColor = document.getElementById("color").value;
+ 	var newCarSeats = document.getElementById("seats").value;
+ 	var newCarPrice = document.getElementById("price").value;
+ 	var newCarImages = document.getElementById("images").value;
+ 	var newCarDescription = document.getElementById("description").value;
+
+ 	// add car into json
+
+}
+
+// -- - -- - -- - -- - -- - -- - --
+
+function removeCar(){
+
+	var carId = document.getElementById("car_id").value;
+
+	fetch('../Json/cars.json')
+		.then(response => response.json())
+		.then((result) => {remove(result, carId)});
+
+
+}
+
+function remove(data, carId){
+	
+	var carUserId = data[carId-1].owner_id;
+
+	if(carUserId != curUser.id){
+
+		alert("You can't remove this car. (not owner)!");
+
+	}else{
+
+		// remove car from json
+
+	}
+
+}
+
+// -- - -- - -- - -- - -- - -- - --
+
+function changePassword(){
+
+	var oldPassword = document.getElementById("old_password").value;
+	var newPassword = document.getElementById("new_password").value;
+	var confNewPassword = document.getElementById("confirm_password").value;
+
+
+	if(oldPassword != curUser.password){
+		alert("Please enter correct old password");
+	}else if(newPassword != confNewPassword){
+		alert("Password mismatch");
+	}else if(newPassword == oldPassword){
+		alert("You can't use old password!")
+	}else{
+
+		// change password in json
+
+	}
+
+}
+
+
+// -- - -- - -- - -- - -- - -- - --
+
+function changePersonalData(){
+
+	var username = document.getElementById("username").value;
+	var firstName = document.getElementById("frst_name").value;
+	var lastName = document.getElementById("lst_name").value;
+	var phone = document.getElementById("contact").value;
+	var location = document.getElementById("location").value;
+
+
+	// add new personal data into json
+
 }
