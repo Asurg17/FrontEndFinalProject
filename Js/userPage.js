fe1b652973b6c8f1136bc599190ef658ccd8ebee
Asurg17 <48888626+Asurg17@users.noreply.github.com>
2021-02-12 21:59:39 +0400

    bug fix & some modifications

--- Js/userPage.js
+++ Js/userPage.js
@@ -1,7 +1,6 @@
 
 // --------------------------------------------------------------------------------------
 
-
 window.onload = function(){
 
 	var url = document.location.href, 
@@ -61,19 +60,19 @@ function setUserInfo(data, id){
 
 	curUser = data[id];
 
-	// var username = curUser.username;
-	// var whole_name = curUser.first_name + " " + curUser.last_name;
-	// var contact = "(+995) " + curUser.phone;
-	// var location = "Georgia, " + curUser.location;
-	// var raiting = curUser.raiting;
-	// var adverts = curUser.adverts;
+	var username = curUser.username;
+	var whole_name = curUser.first_name + " " + curUser.last_name;
+	var contact = "(+995) " + curUser.phone;
+	var location = "Georgia, " + curUser.location;
+	var raiting = curUser.raiting;
+	var adverts = curUser.adverts;
 
-	// document.getElementById("username").innerHTML = username;
-	// document.getElementById("u_name").innerHTML = whole_name;
-	// document.getElementById("u_contact").innerHTML = contact;
-	// document.getElementById("u_location").innerHTML = location;
-	// document.getElementById("u_raiting").innerHTML = raiting;
-	// document.getElementById("u_adverts").innerHTML = adverts;
+	document.getElementById("username").innerHTML = username;
+	document.getElementById("u_name").innerHTML = whole_name;
+	document.getElementById("u_contact").innerHTML = contact;
+	document.getElementById("u_location").innerHTML = location;
+	document.getElementById("u_raiting").innerHTML = raiting;
+	document.getElementById("u_adverts").innerHTML = adverts;
 
 }
 
@@ -147,7 +146,21 @@ function add(data){
  	var newCarImages = document.getElementById("images").value;
  	var newCarDescription = document.getElementById("description").value;
 
+ 	// if some rows are empty
+ 	if(newCarName == "" || newCarManufacturer == "" || newCarModel == "" || newCarModelYear == "" ||
+ 	   newCarCategory == "" || newCarEngine == "" || newCarTransmission == "" || newCarHelm == "" || 
+ 	   newCarFuel == "" || newCarColor == "" || newCarSeats == "" || newCarPrice == ""){
+
+ 		alert("All rows were not fille. Please fill all rows correctly and try again!");
+
+ 	}else{
+
+ 		document.getElementById("f_add_car").reset();
+
+ 	}
+
  	// add car into json
+
 
 }
 
@@ -157,24 +170,38 @@ function removeCar(){
 
 	var carId = document.getElementById("car_id").value;
 
-	fetch('../Json/cars.json')
-		.then(response => response.json())
-		.then((result) => {remove(result, carId)});
+	document.getElementById("f_remove_car").reset();
 
+
+	if(carId != ""){
+		fetch('../Json/cars.json')
+			.then(response => response.json())
+			.then((result) => {remove(result, carId)});
+	}else{
+		alert("Please enter car id, wich you want to remove");
+	}
 
 }
 
 function remove(data, carId){
+
 	
-	var carUserId = data[carId-1].owner_id;
 
-	if(carUserId != curUser.id){
-
-		alert("You can't remove this car. (not owner)!");
-
+	if(data[carId-1] == null){
+		alert("Enter correct car id!");
 	}else{
 
-		// remove car from json
+		var carUserId = data[carId-1].owner_id;;
+		
+		if(carUserId != curUser.id){
+
+			alert("You can't remove this car. (not owner)!");
+
+		}else{
+
+			// remove car from json
+
+		}
 
 	}
 
@@ -188,6 +215,7 @@ function changePassword(){
 	var newPassword = document.getElementById("new_password").value;
 	var confNewPassword = document.getElementById("confirm_password").value;
 
+	document.getElementById("f_password").reset();
 
 	if(oldPassword != curUser.password){
 		alert("Please enter correct old password");
@@ -197,6 +225,11 @@ function changePassword(){
 		alert("You can't use old password!")
 	}else{
 
+		if(newPassword.length < 5 || confNewPassword < 5){
+			alert("Password must be al least five characters long!");
+		}
+
+		
 		// change password in json
 
 	}
@@ -214,6 +247,7 @@ function changePersonalData(){
 	var phone = document.getElementById("contact").value;
 	var location = document.getElementById("location").value;
 
+	document.getElementById("f_personal_data").reset();
 
 	// add new personal data into json
 
